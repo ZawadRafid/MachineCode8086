1c563c656ed05c6a5742bd9d303e56aebfcfafa2
Zawadur Rafid <zawadurrafid7@gmail.com>
2025-07-30 09:41:22 +0600

    Update pattern.asm

--- pattern.asm
+++ pattern.asm
@@ -2,18 +2,53 @@
 .STACK 100H
 .DATA
     VAR DB 0
-    
-    
 .CODE
 
 MAIN PROC
-    MOV AX,@DATA
-    MOV DS,AX
+    MOV AX,0
+    MOV CX,AX
+    MOV BX,AX
+    MOV DX,AX
     
     MOV AH,01H
     INT 21H
     
-    MOV BL,AL
+    MOV DH,1H
+    
+    SUB AL,'0'
+    MOV VAR,AL
+    
+    
+    
+    MOV AH,02H
+    MOV DX,0AH
+    INT 21H
+    MOV DX,0DH
+    INT 21H
+    
+    MOV BL,1
+    
+    OUTER:
+    
+        MOV CL,1
+        INNER:
+            MOV AH,02H
+            MOV DX,42D
+            INT 21H
+            
+            INC CL
+            CMP CL,BL
+            JLE INNER
+        MOV DX,0AH
+        INT 21H    
+        MOV DX,0DH
+        INT 21H
+        INC BL
+        CMP BL,VAR
+        JLE OUTER
+    
+    
+    
     
 
 MAIN ENDP
